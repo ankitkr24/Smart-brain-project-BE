209e4ba3b46c35b4223382ace54db6fef1950326
Ankit Kumar <kankite24@gmail.com>
2020-09-01 23:23:21 +0530

    first commit

--- server.js
+++ server.js
@@ -50,6 +50,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
+    if(!email|| !name || !password){
+        return res.status(400).json('incorrect form submission')
+    }
     const hash = bcrypt.hashSync(password);
 
     db.transaction(trx=>{
